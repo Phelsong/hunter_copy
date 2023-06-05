b8abc8706af8096f7e8acbd3e636230872d71c47
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2023-06-05 18:54:31 -0500

    misc

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-""" TrueFit batch reprocessing script"""
+""" batch copier script"""
 import os
 import sys
 import shutil
@@ -6,13 +6,15 @@ import re
 import csv
 
 
-def batch(in_csv, search_dir, out_dir, verbose=True):
+# ------------------------------------------------------------------------------
+def batch(in_csv, out_dir, search_dir, verbose=True):
     """Copies files based on a (single column csv) list of search elements"""
     work_dir = os.listdir(search_dir)
     case_exists = False
     # ----------------------------
     with open(in_csv, newline="") as csvfile:
         reader = csv.reader(csvfile, delimiter=",", quotechar="|")
+
         for query in reader:
             for item in work_dir:
                 # search thru items in dir for string (id) in CSV
@@ -27,6 +29,7 @@ def batch(in_csv, search_dir, out_dir, verbose=True):
                             + " to: "
                             + f"{out_dir}\\{item}"
                         )
+
             # write not copied
             if case_exists is False:
                 print(f"{query[0]},", file=open(".\\not_found.csv", "a"))
@@ -34,19 +37,21 @@ def batch(in_csv, search_dir, out_dir, verbose=True):
                 case_exists = False
 
 
-# ------------------------------------------------------------------------------
-
 # -----------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     try:
         in_csv = sys.argv[1]
-        search_dir = sys.argv[2]
-        out_dir = sys.argv[3]
+        out_dir = sys.argv[2]
+        search_dir = sys.argv[3]
         # verbose = True if sys.argv[4] == "--verbose" else False
-        batch(in_csv, search_dir, out_dir)
+        batch(
+            in_csv=in_csv,
+            out_dir=out_dir,
+            search_dir=search_dir,
+        )
     except IndexError:
         print(
-            f"Usage: batch.exe <in_csv> <search_dir> <out_dir> < --verbose(optional)>"
+            f"Usage: batch.exe <in_csv> <out_dir> <search_dir> < --verbose(optional)>"
         )
-        print(f"Example: batch.exe .\\csv .\\source_dir_path .\\out_dir_path --verbose")
+        print(f"Example: batch.exe .\\csv .\\out_dir_path .\\search_dir_path --verbose")
